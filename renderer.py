e2233d1b9206cf48643b496b77f3807a79681e8c
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-10-26 23:13:30 +0200

    Suppression de variables inutiles Ajouts de commentaires Corrections de fautes d'orthographes

--- renderer.py
+++ renderer.py
@@ -29,15 +29,10 @@ FACE_TABLE = [[0,1,2,3,0],[1,5,6,2,1],
             [3,4,7,6,2],[4,0,3,7,3],
             [3,2,6,7,4],[0,1,5,4,5]]
 
-FOCAL_LENGHT = 300
 GAME_FPS = 120
 
 ROTATION_ANGLE = 0.01
 ROTATION_MATRICE = [math.cos(ROTATION_ANGLE),-math.sin(ROTATION_ANGLE),math.sin(ROTATION_ANGLE),math.cos(ROTATION_ANGLE)]
-
-ROTATE_X = False
-ROTATE_Y = False
-ROTATE_Z = False
 
 # This is the main game loop, it constantly runs until you press the Q KEY
 # or close the window.
@@ -48,7 +43,7 @@ def main():
     pygame.init()  # Initial Setup
     _WIN = pygame.display.set_mode(SCREENSIZE)
     rotX = rotY = rotZ = False
-    focal_lenght = FOCAL_LENGHT
+    focal_length = 300
 
     sliderRotX = Slider(_WIN, 25, 25, 30, 15, min=0, max=1, step=1, handleColour=(180,0,0), initial=0)
     outputRotX = TextBox(_WIN, 30, 35, 20, 20, fontSize=20, borderThickness=2)
@@ -70,16 +65,18 @@ def main():
     outputFill.setText("FILL")
     outputFill.disable()  # Act as label instead of textbox
 
-    sliderFocalLenght = Slider(_WIN, 25, HEIGHT-40, 200, 15, min=50, max=400, step=2, handleColour=(180,0,0), initial=focal_lenght)
-    outputFocalLenght = TextBox(_WIN, 105, HEIGHT-62, 40, 25, fontSize=20)
-    outputFocalLenght.disable()  # Act as label instead of textbox
+    sliderFocalLength = Slider(_WIN, 25, HEIGHT-40, 200, 15, min=50, max=400, step=2, handleColour=(180,0,0), initial=focal_length)
+    outputFocalLength = TextBox(_WIN, 105, HEIGHT-62, 40, 25, fontSize=20)
+    outputFocalLength.disable()  # Act as label instead of textbox
+
+
 
     # The loop proper, things inside this loop will
     # be called over and over until you exit the window
     running = True
     while running:
         checkEvents()
-        focal_lenght = sliderFocalLenght.getValue()
+        focal_length = sliderFocalLength.getValue()
 
         if(sliderRotX.getValue() == 1) :
             rotX = True
@@ -95,38 +92,49 @@ def main():
         else:
             rotZ = False
 
+        # Fill the background
         _WIN.fill(BACKGROUND_COLOR)
+
+        # Draw the lines
         for line in EDGE_TABLE :
-            drawLine(line, focal_lenght, _WIN)
+            drawLine(line, focal_length, _WIN)
+
+        # Fill the faces
         for face in FACE_TABLE:
             customColor = (42*face[4],42*face[4],42*face[4])
-            drawPolygon(face, customColor, focal_lenght, _WIN)
-        outputFocalLenght.setText(sliderFocalLenght.getValue())
+            drawPolygon(face, customColor, focal_length, _WIN)
+        
+        # Apply rotation and focal length
+        outputFocalLength.setText(sliderFocalLength.getValue())
         rotateOnAxis(rotX, rotY, rotZ)
+
 
         pygame_widgets.update(pygame.event.get())
         pygame.display.update()
         pygame.time.Clock().tick(GAME_FPS)
 
-def drawLine(line, focal_lenght, _WIN):
-    SP1 = project3DOn2DScreen(focal_lenght, VERTEX_TABLE[line[0]])
-    SP2 = project3DOn2DScreen(focal_lenght, VERTEX_TABLE[line[1]])
+
+
+
+def drawLine(line, focal_length, _WIN):
+    SP1 = project3DOn2DScreen(focal_length, VERTEX_TABLE[line[0]])
+    SP2 = project3DOn2DScreen(focal_length, VERTEX_TABLE[line[1]])
     pygame.draw.line(_WIN, LIT, SP1, SP2, 2)
 
-def drawPolygon(face, customColor, focal_lenght, _WIN):
-    polygonPoints = findPolygonPoint(face, focal_lenght)
+def drawPolygon(face, customColor, focal_length, _WIN):
+    polygonPoints = findPolygonPoint(face, focal_length)
     pygame.draw.polygon(_WIN, customColor, polygonPoints, 0)
 
-def findPolygonPoint(face, focal_lenght):
-    P1 = project3DOn2DScreen(focal_lenght,VERTEX_TABLE[face[0]])
-    P2 = project3DOn2DScreen(focal_lenght,VERTEX_TABLE[face[1]])
-    P3 = project3DOn2DScreen(focal_lenght,VERTEX_TABLE[face[2]])
-    P4 = project3DOn2DScreen(focal_lenght,VERTEX_TABLE[face[3]])
+def findPolygonPoint(face, focal_length):
+    P1 = project3DOn2DScreen(focal_length,VERTEX_TABLE[face[0]])
+    P2 = project3DOn2DScreen(focal_length,VERTEX_TABLE[face[1]])
+    P3 = project3DOn2DScreen(focal_length,VERTEX_TABLE[face[2]])
+    P4 = project3DOn2DScreen(focal_length,VERTEX_TABLE[face[3]])
     return [P1,P2,P3,P4]
 
-def project3DOn2DScreen(focal_lenght, Point3D):
-    PointX = (focal_lenght*Point3D[0])/(focal_lenght+Point3D[2])
-    PointY = (focal_lenght*Point3D[1])/(focal_lenght+Point3D[2])
+def project3DOn2DScreen(focal_length, Point3D):
+    PointX = (focal_length*Point3D[0])/(focal_length+Point3D[2])
+    PointY = (focal_length*Point3D[1])/(focal_length+Point3D[2])
     return [PointX+(WIDTH/2),PointY+(HEIGHT/2)]
 
 def rotateOnAxis(rotX, rotY, rotZ):
